64080cb2ad1c76a03d628d341d3a1f88e2c257bf
A <13673553+lewisha@users.noreply.github.com>
2022-03-22 11:15:13 -0400

    this is for testing two yes testing two

--- src/App.js
+++ src/App.js
@@ -2,6 +2,37 @@ import React, { useState, useMemo, useRef } from "react";
 import ReactDom from "react-dom";
 import "./styles.css";
 
-export default function App() {
-  return <div className="App">hello</div>;
+const SearchKeywords = (searchKeywords) => {
+  return <div>what</div>;
+};
+
+const Examp = () => {
+  const [inputVal, setInputVal] = useState("");
+  const [searchKeywords] = useState(["apple ip", "ipad", "iwatch"]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputVal}
+        onChange={(e) => {
+          setInputVal(e.target.value);
+        }}
+      />
+
+      <button className="btn-add">addme</button>
+      <button className="btn-del">delme</button>
+      <SearchKeywords />
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Examp />
+    </div>
+  );
 }
+
+export default App;
